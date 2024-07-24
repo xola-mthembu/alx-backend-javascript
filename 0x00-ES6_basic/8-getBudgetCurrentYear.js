315cc05d65e173bdb7a33ecb2ef7c9568ff1b3a5
xola-mthembu <xola.mthembu@gmail.com>
2024-07-24 11:49:01 -0700

    Task 8: No need to create empty objects

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita
+  };
+}
