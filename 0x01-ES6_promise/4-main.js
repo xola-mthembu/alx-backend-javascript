39db9514e7a47ed45ef9c3b3eb4d88c0ea5a29db
xola-mthembu <xola.mthembu@gmail.com>
2024-07-24 13:32:29 -0700

    Task 4: Simple promise

---
+++ 0x01-ES6_promise/4-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+import signUpUser from "./4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
+}
