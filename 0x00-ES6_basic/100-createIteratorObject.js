aa1f63c3379fd9d9d515e0850c5fdd1db347b8d4
xola-mthembu <xola.mthembu@gmail.com>
2024-07-24 12:06:26 -0700

    Advanced Task 13: Iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+export default function createIteratorObject(report) {
+  return {
+    *[Symbol.iterator]() {
+      for (const department of Object.values(report.allEmployees)) {
+        for (const employee of department) {
+          yield employee;
+        }
+      }
+    }
+  };
+}
