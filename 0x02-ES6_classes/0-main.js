60573301bbcae6313ab02a46d823871c2991fc90
xola-mthembu <xola.mthembu@gmail.com>
2024-07-25 07:31:52 -0700

    Task 0 updated

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+console.log(room._maxStudentsSize);
