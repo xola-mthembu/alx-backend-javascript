afd429e202a935402dade9a29742933741080de0
xola-mthembu <xola.mthembu@gmail.com>
2024-07-25 09:40:01 -0700

    Task 5: Implement Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
