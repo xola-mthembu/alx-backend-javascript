a4ece093d5063abb8deaf2360c8549d4040dad8d
xola-mthembu <xola.mthembu@gmail.com>
2024-07-29 11:45:07 -0700

    6. Set data structure

---
+++ 0x03-ES6_data_manipulation/6-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const setFromArray = require('./6-set');
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+function setFromArray(array) {
+  return new Set(array);
+}
+
+module.exports = setFromArray;
