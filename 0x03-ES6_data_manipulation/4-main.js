c3fc3f464223034a2f6eade766357fdfd0c5be17
xola-mthembu <xola.mthembu@gmail.com>
2024-07-29 11:41:01 -0700

    4. Combine

---
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const getListStudents = require('./0-get_list_students');
+const updateStudentGradeByCity = require('./4-update_grade_by_city');
+
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
+console.log(updateStudentGradeByCity(getListStudents(), 'San Francisco', [{ studentId: 5, grade: 97 }]));
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A'
+      };
+    });
+}
+
+module.exports = updateStudentGradeByCity;
