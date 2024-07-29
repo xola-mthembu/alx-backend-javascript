05a1156f75a90a980a94fb0e11f557ced2cb8b95
xola-mthembu <xola.mthembu@gmail.com>
2024-07-29 11:54:37 -0700

    7. More set data structure

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
+
+module.exports = hasValuesFromArray;
---
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const hasValuesFromArray = require('./7-has_array_values');
+
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
