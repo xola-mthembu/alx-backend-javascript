0d866359e556c158b8648a03e465010e9a61f844
xola-mthembu <xola.mthembu@gmail.com>
2024-07-29 14:51:33 -0700

    Task 0 fixed pt2

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const fetchStudentList = () => {
-	return [
-		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		{ id: 2, firstName: 'James', location: 'Columbia' },
-		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-	];
-};
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
 
-export default fetchStudentList;
+export default getListStudents;
