d2dc758a6a3db6cea903bcb0e74eb8329971b845
xola-mthembu <xola.mthembu@gmail.com>
2024-07-29 11:42:58 -0700

    5. Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-main.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const createInt8TypedArray = require('./5-typed_arrays');
+
+console.log(createInt8TypedArray(10, 2, 89));
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return view;
+}
+
+module.exports = createInt8TypedArray;
