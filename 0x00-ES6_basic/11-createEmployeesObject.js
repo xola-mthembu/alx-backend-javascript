9e7b8cb404a15d665b9239cd61a8048fa2782471
xola-mthembu <xola.mthembu@gmail.com>
2024-07-30 11:02:56 -0700

    Task 11: Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-
 export default function createEmployeesObject(departmentName, employees) {
-  return { [departmentName]: employees };
+  return {
+    [departmentName]: employees,
+  };
 }
--- 0x00-ES6_basic/11-main.js
+++ 0x00-ES6_basic/11-main.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 import createEmployeesObject from './11-createEmployeesObject.js';
 
-console.log(createEmployeesObject("Software", ["Bob", "Sylvie"]));
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
