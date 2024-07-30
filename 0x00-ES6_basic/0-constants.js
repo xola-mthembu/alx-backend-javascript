ed22369c48e334d88d1cb0ec1249e7e5df29ad58
xola-mthembu <xola.mthembu@gmail.com>
2024-07-30 08:41:11 -0700

    Task 0: Const or let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -11,6 +12,5 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
