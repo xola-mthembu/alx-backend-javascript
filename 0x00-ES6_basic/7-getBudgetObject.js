91edcb49827af8114bb686697a95572daf8464eb
xola-mthembu <xola.mthembu@gmail.com>
2024-07-30 10:46:08 -0700

    Task 7: Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
--- 0x00-ES6_basic/7-main.js
+++ 0x00-ES6_basic/7-main.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 import getBudgetObject from './7-getBudgetObject.js';
 
 console.log(getBudgetObject(400, 700, 900));
