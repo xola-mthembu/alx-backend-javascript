61d73aa7be6e25128d6f05c43fcb03735f5d4dc1
xola-mthembu <xola.mthembu@gmail.com>
2024-09-17 12:00:09 -0700

    Task 4: Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, () => {
+  console.log('Server running on port 1245');
+});
+
+module.exports = app;
