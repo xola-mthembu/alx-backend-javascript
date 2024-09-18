8f880c8ba6dabbc3e156191d7d992d3e65ef88c3
xola-mthembu <xola.mthembu@gmail.com>
2024-09-18 13:06:39 -0700

    Task 2: Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai.js');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return the difference of rounded numbers', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+  });
+});
