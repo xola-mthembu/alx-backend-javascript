94e3f207ff76b228658f249b3934a55f2598b47e
xola-mthembu <xola.mthembu@gmail.com>
2024-09-18 12:07:39 -0700

    Task 0: Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', function () {
+  it('should return 4 when inputs are 1 and 3', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when inputs are 1 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when inputs are 1.2 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when inputs are 1.5 and 3.7', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
