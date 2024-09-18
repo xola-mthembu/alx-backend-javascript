2ac993c25403f944a814cb404c5e7dc27847ef34
xola-mthembu <xola.mthembu@gmail.com>
2024-09-18 12:16:26 -0700

    Task 0 updated

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 function calculateNumber(a, b) {
   return Math.round(a) + Math.round(b);
 }
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
+
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function () {
-  it('should return 4 when inputs are 1 and 3', function () {
+describe('calculateNumber', () => {
+  it('should return the sum of rounded numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when inputs are 1 and 3.7', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
