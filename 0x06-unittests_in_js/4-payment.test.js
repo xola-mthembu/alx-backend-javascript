46595a4d7dd691150f9b3353c2e38b8565c1095c
xola-mthembu <xola.mthembu@gmail.com>
2024-09-19 11:48:15 -0700

    4. Stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
-
 const sinon = require('sinon');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('should stub Utils.calculateNumber and log correct message', () => {
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
     const consoleSpy = sinon.spy(console, 'log');
-
+    
     sendPaymentRequestToApi(100, 20);
-
-    expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
-
-    stub.restore();
+    
+    calculateNumberStub.restore();
     consoleSpy.restore();
   });
 });
